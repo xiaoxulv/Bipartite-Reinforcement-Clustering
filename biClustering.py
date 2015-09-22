finish algorithms and experiments, fix bug in tf normalization

biClustering.py
@@ -8,8 +8,8 @@ import kmeans
 def BiClustering(data, k1, k2):
 
     word2Dcluster, doc2D = kmeans.Kmean(data, k1)
-    # base = "docCluster"
-    # s = [base+str(x) for x in xrange(20)]
+    base = "docCluster"
+    s = [base+str(x) for x in xrange(20)]
 
     iter = 0
     while iter < 20:
@@ -24,14 +24,14 @@ def BiClustering(data, k1, k2):
         for i in doc2D.keys():
             word2Dcluster[i] = np.mean(data[doc2D[i]], axis = 0)
 
-        # # for debug here
-        # assign = np.array([-1]*data.shape[0])
-        # for key, value in doc2D.iteritems():
-        #     for v in value:
-        #         assign[v] = key
-        # with open(s[iter], 'w') as dc:
-        #     for idx, item in enumerate(assign):
-        #         dc.write("%d %d\n" % (idx,item))
+        # for debug here
+        assign = np.array([-1]*data.shape[0])
+        for key, value in doc2D.iteritems():
+            for v in value:
+                assign[v] = key
+        with open(s[iter], 'w') as dc:
+            for idx, item in enumerate(assign):
+                dc.write("%d %d\n" % (idx,item))
 
         iter += 1
 

main.py
@@ -36,26 +36,25 @@ for (x,y,z) in zip(row, col, weight):
 
 #print(" %s seconds " % (time.time()-start))
 
-# # normalize tf here
-# M = (1.0*M.transpose()/M.sum(axis=1)).transpose()
-#
-# # read df here
-# idf = []
-# with open("HW2_dev.df") as df:
-#     for line in df:
-#         line = line.strip()
-#         idf.append(int(line.split(":")[1]))
-# for i, x in enumerate(idf):
-#    idf[i] = math.log(M.shape[0]/x+1)
-#
-# for x in xrange(M.shape[1]):
-#     M[:,x] = M[:,x] * idf[x]
+# normalize tf here
+M = (M.transpose()/np.linalg.norm(M, axis=1)).transpose()
+# read df here
+idf = []
+with open("HW2_dev.df") as df:
+    for line in df:
+        line = line.strip()
+        idf.append(int(line.split(":")[1]))
+for i, x in enumerate(idf):
+   idf[i] = math.log(M.shape[0]/x+1)
+
+for x in xrange(M.shape[1]):
+    M[:,x] = M[:,x] * idf[x]
 
 
 
 #kmeans.Kmean(M,50)
 #kmeans.Kmean(M.transpose(),200)
-doc2D, word2W = biClustering.BiClustering(M, 200, 800)
+doc2D, word2W = biClustering.BiClustering(M, 150, 800)
 print(" %s seconds " % (time.time()-start))
 print len(doc2D)
 
@@ -90,15 +89,15 @@ with open("dev_wordCluster.txt", 'w') as dc:
         dc.write("%s %d\n" % (wordDict[idx],item))
 
 
-# f1 store for each iteration
-# res = []
-# base = "docCluster"
-# s = [base+str(x) for x in xrange(20)]
-# for x in xrange(20):
-#     temp = newEval.getF1("HW2_dev.gold_standards",s[x])
-#     print temp
-#     res.append(temp)
-# res = np.array(res)
-# print ("F1 mean is %f" % np.mean(res))
+#f1 store for each iteration
+res = []
+base = "docCluster"
+s = [base+str(x) for x in xrange(20)]
+for x in xrange(20):
+    temp = newEval.getF1("HW2_dev.gold_standards",s[x])
+    print temp
+    res.append(temp)
+res = np.array(res)
+print ("F1 mean is %f" % np.mean(res))
 
 